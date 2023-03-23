4ad456e898a770c13bdd7fa6ff6075447acaa626
ds-mansi <mansi.gurnani@dotsquares.com>
2023-03-23 16:29:38 +0530

    folder added

---
+++ src/config/answersHeadlessConfig.ts
@@ -0,0 +1,20 @@
+
+export const AnswerExperienceConfig = {
+  limit:3,
+  locale: "en",
+  apiKey : "77c136a21f9ff726cadb951fc0194e11",
+  verticalKey : "locations",
+  experienceKey : "vista-optical",
+  experienceVersion: "STAGING",
+  locationRadius: 804672,
+  sessionTrackingEnabled: true,
+  endpoints: {
+    universalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query",
+    verticalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query",
+    questionSubmission: "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion",
+    universalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete",
+    verticalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete",
+    filterSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch",
+
+  }
+}
