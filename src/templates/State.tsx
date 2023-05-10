f5268b9d900229996e94623f3c154fa4e7ec430b
ds-mansi <mansi.gurnani@dotsquares.com>
2023-05-10 17:03:59 +0530

    updated

--- src/templates/State.tsx
+++ src/templates/State.tsx
@@ -252,7 +252,7 @@ const region: Template<TemplateRenderProps> = ({
   var sortedChildren = dm_directoryChildren?.sort(function (a: any, b: any) {
     var a = a.name;
     var b = b.name;
-    return a < b ? -1 : a > b ? 1 : 0;
+    // return a < b ? -1 : a > b ? 1 : 0;
   });
 
   let slugString = "/";
